dd71e9cdb91cba65521f12b0cb83b1ac3efacd5f
K.M.D.Priyashan <kmdpriyashanofficials2001@gmail.com>
2026-07-03 10:28:06 +0530

    Create the data and server.js file ✏️

---
+++ Day-02/data.js
@@ -0,0 +1,7 @@
+const users = [
+    {id: 1, name:"Dulash", email: "dpkonarauni2001@gmail.com"},
+    {id: 2, name:"Githmi", email: "mkdgthathsarani@gmail.com"},
+    {id: 3, name:"Tharindu", email: "tharindu@example.com"}
+];
+
+module.exports = {users};
