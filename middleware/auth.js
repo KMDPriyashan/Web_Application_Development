62f42b1784e57cbb7b52bc53221f63ee688d478b
K.M.D.Priyashan <kmdpriyashanofficials2001@gmail.com>
2026-08-04 23:19:48 -0400

    Create the Auth.js file include the authetication functions. ✅

---
+++ middleware/auth.js
@@ -0,0 +1,27 @@
+const  jwt = require('jsonwebtoken');
+
+const security_key = 'hict3202-super-script';
+
+function verifytoken (req, res, next){
+    const authheader = req.headers['authentication'];
+
+    if(!authheader){
+        return res.status(401).json({
+            message: 'no token provided , please login in first'
+        });
+    }
+
+    const token = authheader.split(' ')[1];
+
+    try{
+        const decode = jwt.verify(token, security_key);
+        req.user = decode;
+        next();
+    } catch (error){
+        return res.status(403).json({
+            message: 'invalid or expired token..!'
+        })
+    }
+}
+
+module.exports = {verifytoken , security_key};
