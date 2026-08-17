f24a2acf1974e3121a0d912a6b36e88cd6bc26c1
K.M.D.Priyashan <kmdpriyashanofficials2001@gmail.com>
2026-08-17 19:50:42 -0400

    Create the databse Files doe the demo data. 🔥

---
+++ database/appointment.js
@@ -0,0 +1,7 @@
+const appointment = [
+    {id: 1, patient: 'kamal', reson: 'fever'},
+    {id: 2, patient: 'nimal', reson: 'checkup'},
+    {id: 3, patient: 'sunil', reson: 'follow-up'}
+];
+
+module.exports = appointment;
